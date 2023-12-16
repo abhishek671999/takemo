5c5e689773922ae9b88e1d856553b927ca5dd043
Abhishek abhishek_a_1999 <abhishekakkakkannavar@gmail.com>
2023-12-16 12:20:05 +0530

    sync server

--- src/app/shared/site-variable.ts
+++ src/app/shared/site-variable.ts
@@ -3,9 +3,10 @@ import { Token } from "@angular/compiler"
 import { inject } from "@angular/core"
 import { CookieService } from "ngx-cookie-service"
 
-export let host = 'http://65.20.75.191:8000/api/v1/' // local test
+// export let host = 'http://65.20.75.191:8001/api/v1/' // local test
 //export let host = 'https://test.takemo.in/api/v1/' // Demo test
-// export let host = 'https://takemo.in/api/v1/' //Prod 
+//export let host = 'https://takemo.in/api/v1/' //Prod 
+export let host = 'https://takemo.in/api/v1/' //Prod 
 
 export function getToken(){
     this.cookieService = inject(CookieService)
