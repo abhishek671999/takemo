5169c2957f64832f84b0cb49418900db7280f007
Abhishek abhishek_a_1999 <abhishekakkakkannavar@gmail.com>
2024-01-05 17:14:20 +0530

    Change vapid

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -8,6 +8,6 @@ export const environment = {
     messagingSenderId: '301672656095',
     appId: '1:301672656095:web:12c68c1214539797454d6a',
     measurementId: 'G-BBCH2PZL60',
-    vapidKey: 'BAqGsH0PQK-aNUPAhzM-fMOHNqgkBSErbIbNvySYXfuM5WVWks_ZhJIem5neEK5_6vA5YZq_0Gi5xfV0FWSN710'
+    vapidKey: 'BFNxlyFA5iIikRaaph7-YYNATe-uMpyTuJALB0x4QZ0lHWWOWKGUHzCY2NYbzZdRrCtpmhls_Ub4YLjsGpEUZJ8'
   },
 };
