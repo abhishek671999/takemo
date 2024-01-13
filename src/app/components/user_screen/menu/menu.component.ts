1a29030b60e855ab4f9fad2cb97bae239c6222f2
Abhishek abhishek_a_1999 <abhishekakkakkannavar@gmail.com>
2024-01-13 13:02:06 +0530

    New menu

--- src/app/components/user_screen/menu/menu.component.ts
+++ src/app/components/user_screen/menu/menu.component.ts
@@ -25,26 +25,33 @@ export class MenuComponent {
     private _dialog: MatDialog,
     private _orderService: OrdersService
   ) {}
+  
 
-  menu_response: any;
+  public menu;
   showSpinner = true;
   restaurant_id: number;
+  public summary;
+
 
   ngOnInit() {
     this.showSpinner = true;
+    this.summary = {
+      amount: 0,
+      itemList: []
+    }
     this._route.paramMap.subscribe((params: ParamMap) => {
       this.restaurant_id = parseInt(params.get('id'));
       this._menuService.getMenu(this.restaurant_id).subscribe(
         (data) => {
-          this.menu_response = data
-          console.log(this.menu_response);
-          this.menu_response.menu.map((category) => {
-            category.category.items.filter(
-              (element) => element.is_available == true
-            );
-          });
-          this.setQuantity();
-          this.showSpinner = false;
+          this.menu = data['menu'];
+        this.menu.map((category) => {
+          category.category.items.filter(
+            (element) => element.is_available == true
+          );
+        });
+        this.setQuantity();
+        this.showOnlyFirstCategory()
+        this.showSpinner = false;
         },
         (error) => {
           console.log('Error while getting menu: ', error)
@@ -53,34 +60,121 @@ export class MenuComponent {
         }
       );
     });
-
-    
   }
 
   setQuantity() {
-    console.log('Setting quantity:', this.menu_response);
-    this.menu_response.menu.forEach((category) => {
+    console.log('Setting quantity:', this.menu);
+    this.menu.forEach((category) => {
       category.category.items.forEach((item) => {
         item.quantity = 0;
       });
     });
   }
 
+  showOnlyFirstCategory(){
+    setTimeout(() => {
+      console.log('trying to show only first element')
+    let allCategoryBlock = Array.from(document.getElementsByClassName('category-wrapper') as HTMLCollectionOf<HTMLElement>)
+    allCategoryBlock.forEach( (ele, index) => {
+      if(index == 0){
+        ele.classList.add('show')
+        ele.classList.remove('hidden')
+      }else{
+        ele.classList.remove('show')
+        ele.classList.add('hidden')
+      }
+    }
+    )
+
+    let allCategoryBar = Array.from(document.getElementsByClassName('category-bar-items') as HTMLCollectionOf<HTMLElement>)
+    allCategoryBar.forEach( (ele, index) => {
+      if(index == 0){
+        ele.classList.add('active')
+      }else{
+        ele.classList.remove('active')
+      }
+    })
+    }, 10);
+      
+  }
+
   addItem(item) {
+    console.log(item)
+    let itemAdded = this.summary.itemList.find( x => x.id == item.id)
+    if(!itemAdded){
+      this.summary.itemList.push(item)
+    }
     if (item.quantity < 10) {
       item.quantity += 1;
-      this.amount += item.price;
-    }
-  }
-  subItem(item) {
-    if (item.quantity > 0) {
-      item.quantity -= 1;
-      this.amount -= item.price;
+      this.summary.amount += item.price;
     }
   }
 
+  subItem(item) {
+    if (item.quantity > 0) {
+      item.quantity -= 1;
+      this.summary.amount -= item.price;
+    }
+    if(item.quantity == 0){
+      this.summary.itemList = this.summary.itemList.filter( x => x.id != item.id)
+    }
+  }
+
+
+  categoryClickEventHandler(category){
+    category = category.replace(' ', '')
+    console.log(category)
+    let allCategoryBlock = Array.from(document.getElementsByClassName('category-wrapper')  as HTMLCollectionOf<HTMLElement>)
+    console.log(allCategoryBlock)
+    allCategoryBlock.forEach(element => {
+      element.classList.remove('show')
+      element.classList.add('hidden')
+    });
+    let categoryBlock = document.getElementById(category)
+    categoryBlock.classList.add('show')
+    categoryBlock.classList.remove('hidden')
+
+    let allCategoryBar = Array.from(document.getElementsByClassName('category-bar-items') as HTMLCollectionOf<HTMLElement>)
+    allCategoryBar.forEach( ele => {
+      console.log('Cateogry contains', ele.classList.contains(category))
+      if(ele.classList.contains(category)){
+        ele.classList.add('active')
+      }else{
+        ele.classList.remove('active')
+      }
+    })
+  }
+
+  clearSummary(){
+    this.summary.amount = 0
+    this.summary.itemList.forEach( item => {
+      item.quantity = 0
+    })
+    this.summary.itemList = []
+  }
+  
+
+  preparePlaceOrderBody(){
+    let itemList = []
+    this.summary.itemList.forEach( ele => {
+      itemList.push(
+        {
+          item_id: ele.id,
+          name: ele.name,
+          quantity: ele.quantity,
+          price: ele.price
+        }
+      )
+    })
+    let body = {   
+      "order_list": itemList,
+      "restaurant_id": this.restaurant_id,
+    }
+    return body
+  }
+
   prepareSummary() {
-    this.menu_response.menu.forEach((category) => {
+    this.menu.forEach((category) => {
       category.category.items.forEach((item) => {
         if (item.quantity) {
           let itemSummary = {
@@ -97,7 +191,7 @@ export class MenuComponent {
     this.orderList.restaurant_id = this.restaurant_id;
     console.log(this.orderList);
     let dialogRef = this._dialog.open(ConfirmationDialogComponent, {
-      data: this.orderList,
+      data: this.summary,
     });
     dialogRef.afterClosed().subscribe((result) => {
       console.log('Result from dialog component: ', result);
