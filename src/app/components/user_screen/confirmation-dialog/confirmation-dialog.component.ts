c25e9db64ae99361ec8e824a73060ee0274cc68a
Abhishek abhishek_a_1999 <abhishekakkakkannavar@gmail.com>
2024-03-01 22:37:56 +0530

    improve user menu

--- src/app/components/user_screen/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/components/user_screen/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,10 @@
-import {Component, Inject} from '@angular/core'
-import {MatDialog, MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { OrdersService } from 'src/app/shared/services/orders/orders.service';
 import { SuccessMsgDialogComponent } from '../../shared/success-msg-dialog/success-msg-dialog.component';
@@ -9,16 +14,16 @@ import { ErrorMsgDialogComponent } from '../../shared/error-msg-dialog/error-msg
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.css'],
-
 })
 export class ConfirmationDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public pickedItems, 
-  public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-  private __ordersService: OrdersService,
-  private dialog: MatDialog,
-  private _router: Router
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public summary,
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    private __ordersService: OrdersService,
+    private dialog: MatDialog,
+    private _router: Router
   ) {
-    console.log('Picked items: ', pickedItems)
+    console.log('Picked items: ', summary);
   }
 
   public isPayment;
@@ -28,115 +33,219 @@ export class ConfirmationDialogComponent {
   public platformFeeAmount;
   public paybyWalletCheck;
   public walletPayMessage;
-  public isWalletPayment = false
+  public isWalletPayment = false;
+  public restaurantParcel = false;
 
-  ngOnInit(){
+  ngOnInit() {
+    this.dialogRef.updateSize('100vw', '80vh')
     this.__ordersService.checkIfPaymentRequired().subscribe(
-      data => {
-        (console.log(data))
-        this.isPayment = data['payment_required']
-        this.totalAmount = this.pickedItems.amount
-        if(data['tax_inclusive']){
-          console.log('INcluding tax')
-          this.platformFee['platform_fee_percentage'] = data['platform_fee_percentage']
-          this.platformFee['platform_fee_gst_percentage'] = data['platform_fee_gst_percentage']
-          this.platformFee['platformFeeAmount'] = ((this.platformFee['platform_fee_percentage'] * this.totalAmount) 
-          * (1 + this.platformFee['platform_fee_gst_percentage']))
-          this.totalAmount = this.totalAmount + this.platformFee['platformFeeAmount']
-          this.roundOffAmount = this.totalAmount.toFixed(2) - this.totalAmount
+      (data) => {
+        console.log(data);
+        this.isPayment = data['payment_required'];
+        this.totalAmount = this.summary.amount;
+        if (data['tax_inclusive']) {
+          console.log('INcluding tax');
+          this.platformFee['platform_fee_percentage'] =
+            data['platform_fee_percentage'];
+          this.platformFee['platform_fee_gst_percentage'] =
+            data['platform_fee_gst_percentage'];
+          this.platformFee['platformFeeAmount'] =
+            this.platformFee['platform_fee_percentage'] *
+            this.totalAmount *
+            (1 + this.platformFee['platform_fee_gst_percentage']);
+          this.totalAmount =
+            this.totalAmount + this.platformFee['platformFeeAmount'];
+          this.roundOffAmount = this.totalAmount.toFixed(2) - this.totalAmount;
 
-          this.platformFeeAmount = this.platformFee['platformFeeAmount'].toFixed(2)
-          this.roundOffAmount = this.roundOffAmount.toFixed(3)
-          this.totalAmount = this.totalAmount.toFixed(2)
+          this.platformFeeAmount =
+            this.platformFee['platformFeeAmount'].toFixed(2);
+          this.roundOffAmount = this.roundOffAmount.toFixed(3);
+          this.totalAmount = this.totalAmount.toFixed(2);
         }
-        if(data['wallet_balance'] < this.pickedItems.amount){
-          this.walletPayMessage = 'You have insufficient balance. Available balance is ₹'+ data['wallet_balance']
-        }else{
-          this.walletPayMessage = 'Payment using Wallet. ' + `&nbsp;₹&nbsp;${this.pickedItems.amount} will be deducted from &nbsp;₹&nbsp;${data['wallet_balance']}`
-          this.isWalletPayment = true
+        if (data['wallet_balance'] < this.summary.amount) {
+          this.walletPayMessage =
+            'You have insufficient balance. Available balance is ₹' +
+            data['wallet_balance'];
+        } else {
+          this.walletPayMessage =
+            'Payment using Wallet. ' +
+            `&nbsp;₹&nbsp;${this.summary.amount} will be deducted from &nbsp;₹&nbsp;${data['wallet_balance']}`;
+          this.isWalletPayment = true;
         }
       },
-      error => {
-        console.log(error)
+      (error) => {
+        console.log(error);
       }
-    )
+    );
   }
 
-  onEditButtonClick(){
-    this.dialogRef.close(false)
+  onEditButtonClick() {
+    this.dialogRef.close(false);
   }
 
-  onConfirmButtonClick(){
-    //this.dialogRef.close({mode: 'wallet'})
-    let body ={   
-      "total_amount": this.pickedItems.amount,
-      "order_list":this.pickedItems.items,
-      "restaurant_id": this.pickedItems.restaurant_id
-  }
-    sessionStorage.setItem('total_amount', this.totalAmount)
-    this.__ordersService.createOrders(body).subscribe(
-      data => {
-        console.log('Payment done', data)
-        sessionStorage.setItem('transaction_id', data['transaction_id'])
-        sessionStorage.setItem('order_no', data['order_no'])
-        this.dialog.open(SuccessMsgDialogComponent, {data: {msg: 'Your Order number is: ' + data['order_no'] } })
-        this.dialogRef.close({mode: 'wallet'})
-      },
-      error => {
-        this.dialog.open(ErrorMsgDialogComponent, {data: {msg: error.error.description}})
-        console.log('Error while paying: ', error.error.description)
-      }
-    )
-    
-  }
 
-  onProceedPaymentClick(){
-    let body ={   
-      "total_amount": this.pickedItems.amount,
-      "wallet": false,
-      "order_list":this.pickedItems.items,
-      "restaurant_id": this.pickedItems.restaurant_id
-  }
-  sessionStorage.setItem('total_amount', this.pickedItems.amount)
-  
-    this.__ordersService.createOrders(body).subscribe(
-      data => {
-        sessionStorage.setItem('transaction_id', data['transaction_id'])
-        sessionStorage.setItem('order_no', data['order_no'])
-        sessionStorage.setItem('redirectURL', '/user/myorders')
-        window.location.href = data['payment_url']
-      },
-      error => {
-        this.dialog.open(ErrorMsgDialogComponent, {data: { msg: error.error.description}})
-        console.log('Error while paying: ', error)
-      }
-    )
-    this.dialogRef.close({mode: 'payment'})
-  }
-
-  onProceedwithWalletClick(){
+  preparePlaceOrderBody(wallet=null) {
+    let itemList = [];
+    this.summary.itemList.forEach((ele) => {
+      itemList.push({
+        item_id: ele.id,
+        quantity: ele.quantity + ele.parcelQuantity,
+        parcel_quantity: ele.parcelQuantity,
+      });
+    });
     let body = {
-      "total_amount": this.pickedItems.amount,
-      "wallet": true,
-      "order_list":this.pickedItems.items,
-      "restaurant_id": this.pickedItems.restaurant_id
+      order_list: itemList,
+      restaurant_id: this.summary.restaurant_id,
+    };
+    if(wallet != null){
+      body['wallet'] = wallet
     }
-    this.__ordersService.createOrders(body).subscribe(
-      data => {
-        console.log('Payment done', data)
-        sessionStorage.setItem('transaction_id', data['transaction_id'])
-        sessionStorage.setItem('order_no', data['order_no'])
-        this.dialog.open(SuccessMsgDialogComponent, {data: {msg: 'Your Order number is: ' + data['order_no'] } })
-        this.dialogRef.close({mode: 'wallet'})
-      },
-      error => {
-        this.dialog.open(ErrorMsgDialogComponent, {data: {msg: error.error.description}})
-        console.log('Error while paying: ', error.error.description)
-      }
-    )
+    return body;
   }
 
-  checkValue(){
-    console.log('pay by check wallet check', this.paybyWalletCheck)
+  onConfirmButtonClick() {
+    //this.dialogRef.close({mode: 'wallet'})
+    let body = this.preparePlaceOrderBody()
+    this.__ordersService.createOrders(body).subscribe(
+      (data) => {
+        console.log('Payment done', data);
+        sessionStorage.setItem('transaction_id', data['transaction_id']);
+        sessionStorage.setItem('order_no', data['order_no']);
+        this.dialog.open(SuccessMsgDialogComponent, {
+          data: { msg: 'Your Order number is: ' + data['order_no'] },
+        });
+        this.dialogRef.close({ mode: 'wallet' });
+      },
+      (error) => {
+        this.dialog.open(ErrorMsgDialogComponent, {
+          data: { msg: error.error.description },
+        });
+        console.log('Error while paying: ', error.error.description);
+      }
+    );
+  }
+
+  onProceedPaymentClick() {
+    let body = this.preparePlaceOrderBody(false)
+
+    this.__ordersService.createOrders(body).subscribe(
+      (data) => {
+        sessionStorage.setItem('transaction_id', data['transaction_id']);
+        sessionStorage.setItem('order_no', data['order_no']);
+        sessionStorage.setItem('redirectURL', '/user/myorders');
+        window.location.href = data['payment_url'];
+      },
+      (error) => {
+        this.dialog.open(ErrorMsgDialogComponent, {
+          data: { msg: error.error.description },
+        });
+        console.log('Error while paying: ', error);
+      }
+    );
+    this.dialogRef.close({ mode: 'payment' });
+  }
+
+  onProceedwithWalletClick() {
+    let body = this.preparePlaceOrderBody(true)
+    this.__ordersService.createOrders(body).subscribe(
+      (data) => {
+        console.log('Payment done', data);
+        sessionStorage.setItem('transaction_id', data['transaction_id']);
+        sessionStorage.setItem('order_no', data['order_no']);
+        this.dialog.open(SuccessMsgDialogComponent, {
+          data: { msg: 'Your Order number is: ' + data['order_no'] },
+        });
+        this.dialogRef.close({ mode: 'wallet' });
+      },
+      (error) => {
+        this.dialog.open(ErrorMsgDialogComponent, {
+          data: { msg: error.error.description },
+        });
+        console.log('Error while paying: ', error.error.description);
+      }
+    );
+  }
+
+  checkValue() {
+    console.log('pay by check wallet check', this.paybyWalletCheck);
+  }
+
+  addItem(item) {
+    console.log('Add item: ', item);
+    let itemAdded = this.summary.itemList.find((x) => x.id == item.id);
+    console.log('item added: ', itemAdded, this.summary.itemList);
+    if (itemAdded) {
+      itemAdded.quantity += 1;
+      this.summary.amount += itemAdded.price;
+    } else {
+      item.quantity += 1;
+      this.summary.amount += item.price;
+      this.summary.itemList.push(item);
+    }
+  }
+  subItem(item) {
+    if (item.quantity > 0) {
+      item.quantity -= 1;
+      this.summary.amount -= item.price;
+    }
+    if (item.quantity == 0 && item.parcelQuantity == 0) {
+      this.summary.itemList = this.summary.itemList.filter(
+        (x) => x.id != item.id
+      );
+    }
+  }
+
+  incrementParcelQuantity(item) {
+    item.parcelQuantity += item.quantity;
+    item.quantity = 0;
+    this.summary.amount += 5
+  }
+
+  subParcelItem(item) {
+    if (item.parcelQuantity > 0) {
+      item.parcelQuantity -= 1;
+      this.summary.amount -= item.price;
+      this.summary.amount -= 5
+    }
+    if (item.quantity == 0 && item.parcelQuantity == 0) {
+      this.summary.itemList = this.summary.itemList.filter(
+        (x) => x.id != item.id
+      );
+    }
+  }
+
+  addParcelItem(item) {
+    console.log('Parcel: ', item);
+    let itemAdded = this.summary.itemList.find((x) => x.id == item.id);
+    if (!itemAdded) {
+      this.summary.itemList.push(item);
+    }
+    if (item.parcelQuantity < 10) {
+      item.parcelQuantity += 1;
+      this.summary.amount += item.price;
+      this.summary.amount += 5
+    }
+  }
+
+  calculateItemAmount(item) {
+    return item.price * (item.quantity + item.parcelQuantity);
+  }
+
+  clearItem(item) {
+    console.log(item);
+    this.summary.itemList
+      .filter((x) => x.id == item.id)
+      .forEach((ele) => {
+        this.summary.amount -= ele.quantity * ele.price;
+        ele.quantity = 0;
+        ele.parcelQuantity = 0;
+      });
+    this.summary.itemList = this.summary.itemList.filter(
+      (x) => x.id != item.id
+    );
+  }
+
+  getGrandTotalAmount() {
+    return this.summary.amount;
   }
 }
--- src/app/components/user_screen/menu/menu.component.ts
+++ src/app/components/user_screen/menu/menu.component.ts
@@ -17,7 +17,7 @@ import { OrdersService } from 'src/app/shared/services/orders/orders.service';
 })
 export class MenuComponent {
   amount: number = 0;
-  orderList = { items: [], amount: 0, restaurant_id: null };
+  orderList = { itemList: [], amount: 0, restaurant_id: null };
   constructor(
     private _menuService: MenuService,
     private _route: ActivatedRoute,
@@ -29,7 +29,9 @@ export class MenuComponent {
   menu_response: any;
   showSpinner = true;
   restaurant_id: number;
-
+  menu;
+  hideCategory = true
+  currentCategory = null;
   ngOnInit() {
     this.showSpinner = true;
     this._route.paramMap.subscribe((params: ParamMap) => {
@@ -37,14 +39,16 @@ export class MenuComponent {
       this._menuService.getMenu(this.restaurant_id).subscribe(
         (data) => {
           this.menu_response = data
-          console.log(this.menu_response);
-          this.menu_response.menu.map((category) => {
+          this.menu = data['menu']
+          console.log('Menu response', data);
+          this.menu.map((category) => {
             category.category.items.filter(
               (element) => element.is_available == true
             );
           });
           this.setQuantity();
           this.showSpinner = false;
+          this.showOnlyFirstCategory();
         },
         (error) => {
           console.log('Error while getting menu: ', error)
@@ -52,27 +56,67 @@ export class MenuComponent {
           alert('Error while loading menu')
         }
       );
-    });
-
-    
+    });    
   }
 
   setQuantity() {
     console.log('Setting quantity:', this.menu_response);
-    this.menu_response.menu.forEach((category) => {
+    this.menu.forEach((category) => {
       category.category.items.forEach((item) => {
         item.quantity = 0;
+        item.parcelQuantity = 0;
       });
     });
   }
 
-  addItem(item) {
+  togglehideCategory(){
+    this.hideCategory = !this.hideCategory
+    let categoryBar = document.getElementById('collapsable-category-bar') as HTMLElement;
+    categoryBar.style.zIndex = this.hideCategory ? '5': '0'
+  }
+
+  showOnlyFirstCategory() {
+    setTimeout(() => {
+      let allCategoryBlock = Array.from(
+        document.getElementsByClassName(
+          'category-wrapper'
+        ) as HTMLCollectionOf<HTMLElement>
+      );
+      allCategoryBlock.forEach((ele, index) => {
+        if (index == 0) {
+          this.currentCategory = ele.id
+          ele.classList.add('show');
+          ele.classList.remove('hidden');
+        } else {
+          ele.classList.remove('show');
+          ele.classList.add('hidden');
+        }
+      });
+
+      let allCategoryBar = Array.from(
+        document.getElementsByClassName(
+          'category-bar-items'
+        ) as HTMLCollectionOf<HTMLElement>
+      );
+      allCategoryBar.forEach((ele, index) => {
+        if (index == 0) {
+          ele.classList.add('active');
+        } else {
+          ele.classList.remove('active');
+        }
+      });
+    }, 10);
+  }
+
+  addItem(item,event) {
+    event.stopPropagation()
     if (item.quantity < 10) {
       item.quantity += 1;
       this.amount += item.price;
     }
   }
-  subItem(item) {
+  subItem(item, event) {
+    event.stopPropagation()
     if (item.quantity > 0) {
       item.quantity -= 1;
       this.amount -= item.price;
@@ -84,12 +128,13 @@ export class MenuComponent {
       category.category.items.forEach((item) => {
         if (item.quantity) {
           let itemSummary = {
-            item_id: item.id,
+            id: item.id,
             name: item.name,
             quantity: item.quantity,
             price: item.price,
+            parcelQuantity: item.parcelQuantity
           };
-          this.orderList.items.push(itemSummary);
+          this.orderList.itemList.push(itemSummary);
         }
       });
     });
@@ -106,9 +151,39 @@ export class MenuComponent {
         if(result.mode == 'wallet'){
           this._router.navigate(['/user/myorders'])
         }
-        this.orderList = { items: [], amount: 0, restaurant_id: null };
+        this.orderList = { itemList: [], amount: 0, restaurant_id: null };
       } else {
-        this.orderList = { items: [], amount: 0, restaurant_id: null };
+        this.orderList = { itemList: [], amount: 0, restaurant_id: null };
+      }
+    });
+  }
+
+  categoryClickEventHandler(category) {
+    this.currentCategory = category
+    category = category.replace(' ', '');
+    let allCategoryBlock = Array.from(
+      document.getElementsByClassName(
+        'category-wrapper'
+      ) as HTMLCollectionOf<HTMLElement>
+    );
+    allCategoryBlock.forEach((element) => {
+      element.classList.remove('show');
+      element.classList.add('hidden');
+    });
+    let categoryBlock = document.getElementById(category);
+    categoryBlock.classList.add('show');
+    categoryBlock.classList.remove('hidden');
+
+    let allCategoryBar = Array.from(
+      document.getElementsByClassName(
+        'category-bar-items'
+      ) as HTMLCollectionOf<HTMLElement>
+    );
+    allCategoryBar.forEach((ele) => {
+      if (ele.classList.contains(category)) {
+        ele.classList.add('active');
+      } else {
+        ele.classList.remove('active');
       }
     });
   }
